8f5073fd723c2f3fcfcc795c2d49a3826d569a89
Ben Smith <benasmith278@gmail.com>
2021-01-03 23:19:49 -0500

    Initial Commit

---
+++ src/main/java/com/epicgamertwo/modOfThings/modOfThings.java
@@ -0,0 +1,10 @@
+package com.epicgamertwo.modOfThings;
+
+import net.fabricmc.api.ModInitializer;
+
+public class modOfThings implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
